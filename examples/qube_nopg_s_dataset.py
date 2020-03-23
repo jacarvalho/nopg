1702edd58ffa448ea04b389dfc37a3741b43d199
carvalho <joao.ac.carvalho@gmail.com>
2020-03-23 11:25:36 +0100

    Update qube experiment.

--- examples/qube_nopg_s_dataset.py
+++ examples/qube_nopg_s_dataset.py
@@ -14,8 +14,8 @@ from src.nopg.nopg import NOPG
 from src.dataset.dataset import Dataset
 
 # Use the GPU if available, or if the memory is insufficient use only the CPU
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# DEVICE = torch.device('cpu')
+# DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
 
 ##########################################################################################
 # Create the Environment (MDP)
@@ -37,7 +37,7 @@ os.makedirs(results_dir, exist_ok=True)
 # Load trajectories from file
 filename = '/home/carvalho/Documents/projects/nopg/datasets/qube/15_trajectories.npy'
 dataset = Dataset(results_dir=results_dir)
-dataset.load_trajectories_from_file(filename, n_trajectories=3)
+dataset.load_trajectories_from_file(filename, n_trajectories=8)
 dataset.update_dataset_internal()
 s_band_factor = [15., 15., 15, 15., 1., 1.]
 s_n_band_factor = s_band_factor
@@ -51,9 +51,9 @@ dataset.plot_data_kde(state_labels=mdp._env.observation_space.labels, action_lab
 # Define the Policy Network
 
 policy_params = {'policy_class': 'stochastic',
-                 'neurons': [mdp.s_dim, 50, mdp.a_dim],  # [state_dim, hidden1, ... , hiddenN, action_dim]
+                 'neurons': [mdp.s_dim, 60, mdp.a_dim],  # [state_dim, hidden1, ... , hiddenN, action_dim]
                  'activations': [nn.functional.relu],  # one activation function per hidden layer
-                 'f_out': [lambda x: 2.0 * torch.tanh(x), lambda x: 0.5 * torch.sigmoid(x)],
+                 'f_out': [lambda x: 2.5 * torch.tanh(x), lambda x: 0.5 * torch.sigmoid(x)],
                  'device': DEVICE
                  }
 
@@ -62,9 +62,9 @@ policy = Policy(**policy_params).to(device=DEVICE, dtype=TORCH_DTYPE)
 ##########################################################################################
 # Optimize the policy with NOPG
 
-nopg_params = {'initial_states': np.array([env.reset() for _ in range(10)]),  # For multiple initial states
-               'gamma': 0.999,
-               'MC_samples_stochastic_policy': 5,
+nopg_params = {'initial_states': np.array([env.reset() for _ in range(6)]),  # For multiple initial states
+               'gamma': 0.9991,
+               'MC_samples_stochastic_policy': 8,
                # 'MC_samples_P': 15,
                # 'sparsify_P': {'kl_max': 0.001, 'kl_interval_k': 20, 'kl_repeat_every_n_iterations': 5}
                }
@@ -76,7 +76,7 @@ def optimizer(x): return optim.Adam(x, lr=5e-3)
 evaluation_params = {'eval_mdp': mdp,
                      'eval_every_n': 25,
                      'eval_n_episodes': 1,
-                     'eval_render': True
+                     'eval_render': False
                      }
 
 nopg.fit(n_policy_updates=n_policy_updates, optimizer=optimizer, **evaluation_params)
